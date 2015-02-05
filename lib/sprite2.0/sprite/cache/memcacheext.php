80a2ee95be46732ec38c507a29c37d0296b0b478
oakknight <danielma@kitetea.com>
2015-02-05 19:33:38 +0800

    modify memcached

--- lib/sprite2.0/sprite/cache/memcacheext.php
+++ lib/sprite2.0/sprite/cache/memcacheext.php
@@ -13,9 +13,11 @@ class Memcacheext extends \Memcached {
     public function addServers($list) {
         foreach($list as $row) {
             $this->_hosts[] = $row['host'];
-            parent::addserver($row['host'], $row['port']);
-            if($row['username'])//设置OCS帐号密码进行鉴权
-            {
+            parent::addServer($row['host'], $row['port']);
+            if($row['username'] )//设置OCS帐号密码进行鉴权
+			{
+				parent::setOption(Memcached::OPT_COMPRESSION, false); //关闭压缩功能
+				parent::setOption(Memcached::OPT_BINARY_PROTOCOL, true);//使用binary二进制协议
             	parent::setSaslAuthData($row['username'], $row['pass']); 
             }
             
@@ -24,7 +26,7 @@ class Memcacheext extends \Memcached {
 
     public function set($key, $var, $flag = MEMCACHE_COMPRESSED, $expire = 30) {
         $begin_microtime = Debug::getTime();
-        $ret = parent::set($key, $var, MEMCACHE_COMPRESSED, $expire);
+        $ret = parent::set($key, $var, $expire);
         Debug::cache($this->_hosts, $key, Debug::getTime() - $begin_microtime, $ret);
         return $ret;
     }
