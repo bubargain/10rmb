8613ba74ea42df234c8a6bf22bce046d82d515a3
daniel ma <daniel.ma@10buck.com>
2015-05-27 16:28:31 +0800

    change pic link

--- api/controller/syscontroller.php
+++ api/controller/syscontroller.php
@@ -17,12 +17,12 @@ class syscontroller extends Controller{
 			'amount'=>'6',
 			'utime' => '1432545706',
 			'brands' => array(
-				'1' => array('name'=>'zara','pic_link'=>'http://api.10buck.com/img/logo1.jpg','event_id'=>12) ,
-				'2' => array('name'=>'h&m','pic_link'=>'http://api.10buck.com/img/logo2.jpg','event_id'=>13),
-				'3' => array('name'=>'LV','pic_link'=>'http://api.10buck.com/img/logo3.jpg','event_id'=>14),
-				'4' => array('name'=>'GUESS','pic_link'=>'http://api.10buck.com/img/logo4.jpg','event_id'=>15),
-				'5' => array('name'=>'LAFLOARIN','pic_link'=>'http://api.10buck.com/img/logo5.jpg','event_id'=>17),
-				'6'=> array('name'=>'DNANIEL&KATE','pic_link'=>'http://api.10buck.com/img/logo6.jpg','event_id'=>18),
+				'1' => array('name'=>'zara','pic_link'=>'http://120.26.107.42/img/logo1.jpg','event_id'=>12) ,
+				'2' => array('name'=>'h&m','pic_link'=>'http://120.26.107.42/img/logo2.jpg','event_id'=>13),
+				'3' => array('name'=>'LV','pic_link'=>'http://120.26.107.42/img/logo3.jpg','event_id'=>14),
+				'4' => array('name'=>'GUESS','pic_link'=>'http://120.26.107.42/img/logo4.jpg','event_id'=>15),
+				'5' => array('name'=>'LAFLOARIN','pic_link'=>'http://120.26.107.42/img/logo5.jpg','event_id'=>17),
+				'6'=> array('name'=>'DNANIEL&KATE','pic_link'=>'http://120.26.107.42/img/logo6.jpg','event_id'=>18),
 			)
 		));
 	}
@@ -39,13 +39,13 @@ class syscontroller extends Controller{
 			'amount'=>'7',
 			'utime' => '1432545706',
 			'brands' => array(
-				'1' => array('name'=>'CASUAL','pic_link'=>'http://api.10buck.com/img/logo7.jpg','event_id'=>12,'amount'=>200) ,
-				'2' => array('name'=>'OFFICIAL','pic_link'=>'http://api.10buck.com/img/logo6.jpg','event_id'=>13,'amount'=>22),
-				'3' => array('name'=>'PARTY','pic_link'=>'http://api.10buck.com/img/logo5.jpg','event_id'=>14,'amount'=>11),
-				'4' => array('name'=>'OUTDOOR','pic_link'=>'http://api.10buck.com/img/logo4.jpg','event_id'=>15,'amount'=>9),
-				'5' => array('name'=>'SCHOOOL','pic_link'=>'http://api.10buck.com/img/logo3.jpg','event_id'=>17,'amount'=>200),
-				'6'=> array('name'=>'BEACH','pic_link'=>'http://api.10buck.com/img/logo2.jpg','event_id'=>18,'amount'=>232),
-				'7'=> array('name'=>'PROM','pic_link'=>'http://api.10buck.com/img/logo2.jpg','event_id'=>18,'amount'=>203),		
+				'1' => array('name'=>'CASUAL','pic_link'=>'http://120.26.107.42/img/logo7.jpg','event_id'=>12,'amount'=>200) ,
+				'2' => array('name'=>'OFFICIAL','pic_link'=>'http://120.26.107.42/img/logo6.jpg','event_id'=>13,'amount'=>22),
+				'3' => array('name'=>'PARTY','pic_link'=>'http://120.26.107.42/img/logo5.jpg','event_id'=>14,'amount'=>11),
+				'4' => array('name'=>'OUTDOOR','pic_link'=>'http://120.26.107.42/img/logo4.jpg','event_id'=>15,'amount'=>9),
+				'5' => array('name'=>'SCHOOOL','pic_link'=>'http://120.26.107.42/img/logo3.jpg','event_id'=>17,'amount'=>200),
+				'6'=> array('name'=>'BEACH','pic_link'=>'http://120.26.107.42/img/logo2.jpg','event_id'=>18,'amount'=>232),
+				'7'=> array('name'=>'PROM','pic_link'=>'http://120.26.107.42/img/logo2.jpg','event_id'=>18,'amount'=>203),		
 		)
 		));
 	}
@@ -62,13 +62,13 @@ class syscontroller extends Controller{
 			'amount'=>'7',
 			'utime' => '1432545706',	
 			'brands' => array(
-				'1' => array('name'=>'CUTE','pic_link'=>'http://api.10buck.com/img/logo7.jpg','event_id'=>12,'amount'=>200) ,
-				'2' => array('name'=>'BRIEF','pic_link'=>'http://api.10buck.com/img/logo5.jpg','event_id'=>13,'amount'=>22),
-				'3' => array('name'=>'STREET','pic_link'=>'http://api.10buck.com/img/logo2.jpg','event_id'=>14,'amount'=>11),
-				'4' => array('name'=>'SPORTY','pic_link'=>'http://api.10buck.com/img/logo4.jpg','event_id'=>15,'amount'=>9),
-				'5' => array('name'=>'SEXY','pic_link'=>'http://api.10buck.com/img/logo3.jpg','event_id'=>17,'amount'=>200),
-				'6'=> array('name'=>'PREPPY','pic_link'=>'http://api.10buck.com/img/logo3.jpg','event_id'=>18,'amount'=>232),
-				'7'=> array('name'=>'WORK','pic_link'=>'http://api.10buck.com/img/logo1.jpg','event_id'=>18,'amount'=>203),		
+				'1' => array('name'=>'CUTE','pic_link'=>'http://120.26.107.42/img/logo7.jpg','event_id'=>12,'amount'=>200) ,
+				'2' => array('name'=>'BRIEF','pic_link'=>'http://120.26.107.42/img/logo5.jpg','event_id'=>13,'amount'=>22),
+				'3' => array('name'=>'STREET','pic_link'=>'http://120.26.107.42/img/logo2.jpg','event_id'=>14,'amount'=>11),
+				'4' => array('name'=>'SPORTY','pic_link'=>'http://120.26.107.42/img/logo4.jpg','event_id'=>15,'amount'=>9),
+				'5' => array('name'=>'SEXY','pic_link'=>'http://120.26.107.42/img/logo3.jpg','event_id'=>17,'amount'=>200),
+				'6'=> array('name'=>'PREPPY','pic_link'=>'http://120.26.107.42/img/logo3.jpg','event_id'=>18,'amount'=>232),
+				'7'=> array('name'=>'WORK','pic_link'=>'http://120.26.107.42/img/logo1.jpg','event_id'=>18,'amount'=>203),		
 		)
 		));
 	}
