10fd47c73c04e7da0f1d98a8e1ccdf27a3c24dac
daniel ma <daniel.ma@10buck.com>
2015-06-03 11:46:09 +0800

    [s] change some data in sys/list"

--- api/controller/syscontroller.php
+++ api/controller/syscontroller.php
@@ -37,14 +37,14 @@ class syscontroller extends Controller{
 	public function occasion_list($request,$response){
 		$this->renderJson(array(
 			'amount'=>'7',
-			'utime' => '1432545226',
+			'utime' => '1432545227',
 			'list' => array(
 				'1' => array('name'=>'beach','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/beach.jpg','event_id'=>12,'amount'=>200) ,
 				'2' => array('name'=>'casual','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/casual1.jpg','event_id'=>13,'amount'=>22),
 				'3' => array('name'=>'office','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/office1.jpg','event_id'=>14,'amount'=>11),
-				'4' => array('name'=>'OUTDOOR','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/outdoor.jpg','event_id'=>15,'amount'=>9),
-				'5' => array('name'=>'party','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/party.jpg','event_id'=>17,'amount'=>200),
-				'6'=> array('name'=>'prom','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/prom1.jpg','event_id'=>18,'amount'=>232),
+				'4' => array('name'=>'OUTDOOR','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/outdoor2.jpg','event_id'=>15,'amount'=>9),
+				'5' => array('name'=>'party','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/party2.jpg','event_id'=>17,'amount'=>200),
+				'6'=> array('name'=>'prom','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/prom2.jpg','event_id'=>18,'amount'=>232),
 				'7'=> array('name'=>'school','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/school.jpg','event_id'=>19,'amount'=>203),		
 		)
 		));
@@ -60,17 +60,17 @@ class syscontroller extends Controller{
 	public function style_list($request,$response){
 		$this->renderJson(array(
 			'amount'=>'7',
-			'utime' => '1432543336',	
+			'utime' => '1432543337',	
 			'list' => array(
 				'1' => array('name'=>'bohemian','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/bohemian.jpg','event_id'=>12,'amount'=>200) ,
 				'2' => array('name'=>'BRIEF','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/brief1.jpg','event_id'=>13,'amount'=>22),
-				'3' => array('name'=>'CUTE','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/cute.jpg','event_id'=>14,'amount'=>11),
+				'3' => array('name'=>'CUTE','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/cute2.jpg','event_id'=>14,'amount'=>11),
 				'4' => array('name'=>'preppy','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/preppy.jpg','event_id'=>15,'amount'=>9),
 				'5' => array('name'=>'SEXY','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/sexy.jpg','event_id'=>17,'amount'=>200),
 				'6'=> array('name'=>'sporty','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/sporty.jpg','event_id'=>18,'amount'=>232),
 				'7'=> array('name'=>'street','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/street.jpg','event_id'=>19,'amount'=>203),		
 				'8'=> array('name'=>'vintage','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/vintage.jpg','event_id'=>20,'amount'=>203),		
-				'9'=> array('name'=>'work','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/work.jpg','event_id'=>21,'amount'=>203),		
+				'9'=> array('name'=>'work','pic_link'=>'http://7s1rnv.com1.z0.glb.clouddn.com/work2.jpg','event_id'=>21,'amount'=>203),		
 				
 		)
 		));
